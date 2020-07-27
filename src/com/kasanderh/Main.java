0027ac2b183140556b67b0e8b446a698c1782527
Kasander Hanssen <kasanderh@gmail.com>
2020-07-27 08:35:56 +0200

    More on Streams

--- src/com/kasanderh/Main.java
+++ src/com/kasanderh/Main.java
@@ -5,6 +5,7 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -34,5 +35,16 @@ public class Main {
                 .filter(s->s.startsWith("G"))
                 .sorted()
                 .forEach(System.out::println);
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26","I17", "I29", "O71");
+        // concatenate = join two strings
+        Stream<String> concatStream = Stream.concat(ioNumberStream,inNumberStream);
+        System.out.println("=======================");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
+
     }
 }
